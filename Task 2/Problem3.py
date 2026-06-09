dc87db839115367da4aa5b824b0ee53cd462fdd1
Kongkon Dey Kabya <150504766+0kabya0@users.noreply.github.com>
2026-06-09 16:22:01 +0600

    Implement reverse and complement for DNA sequence

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,17 @@
+pattern = input()
 
+reverse_pattern = pattern[::-1]
+
+result= ""
+
+for ch in reverse_pattern:
+  if ch == "A":
+    result+="T"
+  elif ch == "T":
+    result+="A"
+  elif ch == "C":
+    result+="G"
+  elif ch == "G":
+    result+="C"
+
+print(result)
