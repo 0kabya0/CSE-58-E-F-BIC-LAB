946cef830d609da1ec3547978428442d1e7a0898
Kongkon Dey Kabya <150504766+0kabya0@users.noreply.github.com>
2026-06-09 16:13:53 +0600

    Implement substring frequency analysis
    Add functionality to find and print most common substrings of a given length.

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,19 @@
+string = input()
+k = int(input())
 
+patterns = []
+
+for i in range(len(string)-k+1):
+  patterns.append(string[i:i+k])
+
+max_count = 0
+for pattern in patterns:
+  count = patterns.count(pattern)
+  if count > max_count:
+    max_count = count
+printed = []
+
+for pattern in patterns:
+  if patterns.count(pattern) == max_count and pattern not in printed:
+    print(pattern, end=" ")
+    printed.append(pattern)
