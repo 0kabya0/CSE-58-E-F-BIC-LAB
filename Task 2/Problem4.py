f74bfc9278b6ec229fd0beb433c986ef42290f52
Kongkon Dey Kabya <150504766+0kabya0@users.noreply.github.com>
2026-06-16 12:08:41 +0600

    Find pattern positions in genome string

--- Task 2/Problem4.py
+++ Task 2/Problem4.py
@@ -1,1 +1,10 @@
+pattern = input().strip()
+genome = input().strip()
 
+positions = []
+
+for i in range(len(genome) - len(pattern) + 1):
+    if genome[i:i + len(pattern)] == pattern:
+        positions.append(str(i))
+
+print(" ".join(positions))
